bf9f12452f4d2d1fa93358669abec6e02e0f782e
MJ Kim <116478809+KMJ-safte@users.noreply.github.com>
2025-06-13 20:45:37 +0900

    favicon 바꿈

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,7 @@ export const siteConfig: SiteConfig = {
 	},
 	favicon: [
 		{
-			src: "assets/images/favicon.png", // Path of the favicon, relative to the /src directory. Relative to the /public directory if it starts with '/'
+			src: "assets/images/favicon.ico", // Path of the favicon, relative to the /src directory. Relative to the /public directory if it starts with '/'
 			theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
 			sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
 		},
